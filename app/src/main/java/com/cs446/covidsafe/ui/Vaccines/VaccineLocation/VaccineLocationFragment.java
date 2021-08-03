4cbf7d777f8458cdb1153f07635a8cab11f4e176
“Virus4” <virus4@163.com>
2021-08-02 22:30:43 -0400

    remove hardcode

--- app/src/main/java/com/cs446/covidsafe/ui/Vaccines/VaccineLocation/VaccineLocationFragment.java
+++ app/src/main/java/com/cs446/covidsafe/ui/Vaccines/VaccineLocation/VaccineLocationFragment.java
@@ -1,83 +1,202 @@
 package com.cs446.covidsafe.ui.Vaccines.VaccineLocation;
 
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+
+
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.cs446.covidsafe.R;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class VaccineLocationFragment extends Fragment {
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.maps.model.OpeningHours;
+import com.google.maps.model.PlacesSearchResult;
+import com.cs446.covidsafe.model.NearbySearch;
+
+
+
+
+
+
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+
+public class VaccineLocationFragment extends Fragment  implements  OnMapReadyCallback {
 
     public VaccineLocationFragment() {
         // Required empty public constructor
     }
+    private PlacesClient placesClient;
+    public LatLng currentLatLng;
+    private static final String TAG = VaccineLocationFragment.class.getSimpleName();
+    private GoogleMap map;
+    private CameraPosition cameraPosition;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        //43.47425619407127, -80.5362798774214
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng waterloo = new LatLng(43.4643, -80.5204);
-            CameraUpdateFactory.zoomTo(15);
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng defaultLocation = new LatLng(43.47425619407127, -80.5362798774214);
+    private static final int DEFAULT_ZOOM = 13;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
 
-            googleMap.addMarker(new MarkerOptions()
-                    .position(waterloo)
-                    .title("My place")
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-
-            //43.47195552353127, -80.53870003230735
-            LatLng PharmasaveCampus = new LatLng(43.47195552353127, -80.53870003230735);
-            googleMap.addMarker(new MarkerOptions()
-                    .position(PharmasaveCampus)
-                    .title("PharmasaveCampus"));
-            //43.47581152967209, -80.52459460788678
-            LatLng KWUniversityPharmacy = new LatLng(43.47581152967209, -80.52459460788678);
-            googleMap.addMarker(new MarkerOptions()
-                    .position(KWUniversityPharmacy)
-                    .title("KWUniversityPharmacy"));
-            //43.486168172976534, -80.54034052263884
-            LatLng AlphaMedPharmacy = new LatLng(43.486168172976534, -80.54034052263884);
-            googleMap.addMarker(new MarkerOptions()
-                    .position(AlphaMedPharmacy)
-                    .title("AlphaMedPharmacy"));
-            //43.462221187511126, -80.53691257021013
-            LatLng PharmasaveWestmountPlace = new LatLng(43.462221187511126, -80.53691257021013);
-            googleMap.addMarker(new MarkerOptions()
-                    .position(PharmasaveWestmountPlace)
-                    .title("PharmasaveWestmountPlace"));
-            //43.46424170820742, -80.52349209534587
-            LatLng ShoppersDrugMart = new LatLng(43.46424170820742, -80.52349209534587);
-            googleMap.addMarker(new MarkerOptions()
-                    .position(ShoppersDrugMart)
-                    .title("ShoppersDrugMart"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(waterloo,13));
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location lastKnownLocation;
 
 
+    // Keys for storing activity state.
+    // [START maps_current_place_state_keys]
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to
+     * install it inside the SupportMapFragment. This method will only be triggered once the
+     * user has installed Google Play services and returned to the app.
+     */
+    @Override
+    //
+    public void onMapReady(GoogleMap googleMap) {
+
+        this.map = googleMap;
+
+        // Prompt the user for permission.
+        getLocationPermission();
+        // [END_EXCLUDE]
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation();
+
+
+    }
+    private void getNearByPharmacy()
+    {
+        com.google.maps.model.LatLng tmp =
+                new com.google.maps.model.LatLng(lastKnownLocation.getLatitude(),
+                        lastKnownLocation.getLongitude());
+        PlacesSearchResult[] placesSearchResults = new NearbySearch().run(tmp).results;
+
+        double lat1 = 0;
+        double lng1 = 0;
+        String name = "";
+        for(int i = 0; i < placesSearchResults.length; i++ )
+        {
+            name = placesSearchResults[i].name;
+            lat1 = placesSearchResults[i].geometry.location.lat;
+            lng1 = placesSearchResults[i].geometry.location.lng;
+
+            map.addMarker(new MarkerOptions().position(new LatLng(lat1, lng1)) .title(name) );
         }
-    };
+    }
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                                getNearByPharmacy();
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            map.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+    private void updateLocationUI() {
+        if (map == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
 
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -90,12 +209,17 @@ public class VaccineLocationFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+            mapFragment.getMapAsync(this);
         }
     }
+
+
+
 
 }
