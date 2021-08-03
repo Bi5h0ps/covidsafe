4432f6e5760caaf994f401947e6f723611291cbf
Jiahan Xi <doomerxe@gmail.com>
2021-08-03 14:58:07 -0400

    Revert "remove hardcode"

--- app/src/main/java/com/cs446/covidsafe/ui/Main/MainActivity.java
+++ app/src/main/java/com/cs446/covidsafe/ui/Main/MainActivity.java
@@ -8,19 +8,15 @@ import android.os.Bundle;
 import android.view.Window;
 import android.widget.RadioGroup;
 
-import com.cs446.covidsafe.BuildConfig;
 import com.cs446.covidsafe.R;
 import com.cs446.covidsafe.ui.Covid.CovidFragment;
 import com.cs446.covidsafe.ui.Vaccines.VaccineFragment;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
 
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
 
@@ -41,10 +37,6 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         showFragment(1);
         radioGroup.check(R.id.rb_1);
         radioGroup.setOnCheckedChangeListener(this);
-        // Initialize the SDK
-        if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), BuildConfig.PLACES_API_KEY);
-        }
     }
 
     private void showFragment(int page) {
